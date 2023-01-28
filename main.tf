1ee95563a28c34af5fbb138a58ef91e5ac84e14a
Marco Franchi <marco_franchi@outlook.com>
2023-01-28 13:19:06 +0200

    Fixed syn error

--- main.tf
+++ main.tf
@@ -41,9 +41,9 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
+  version = "~> 6.0"
 
   name = "blog-alb"
 
@@ -53,19 +53,12 @@ module "alb" {
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
-
   target_groups = [
     {
       name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   ]
 
@@ -84,12 +77,12 @@ module "alb" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.17.1"
-  
-  name                 = "blog"
-  vpc_id               = module.blog_vpc.vpc_id
-  ingress_rules        = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks  = ["0.0.0.0/0"]
-  egress_rules         = ["all-all"]
-  egress_cidr_blocks   = ["0.0.0.0/0"]
- }
+  version = "4.13.0"
+
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
