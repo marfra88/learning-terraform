834e1750728690faf07aaa0f9eda60ea03e1421a
Marco Franchi <marco_franchi@outlook.com>
2023-01-28 13:11:45 +0200

    Fixed vpc_id

--- main.tf
+++ main.tf
@@ -86,8 +86,8 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
   
-  name   = "blog"
-  vpc_id = module.vpc.public_subnets[0]
+  name                 = "blog"
+  vpc_id               = module.blog_vpc.vpc_id
   ingress_rules        = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks  = ["0.0.0.0/0"]
   egress_rules         = ["all-all"]
